ce601cd8d4cca1e62c78f6bd652c2e4d24a69367
Akash Suryawanshi <166898738+Akkii5@users.noreply.github.com>
2025-04-23 12:30:31 +0530

    Delete backend directory

--- backend/config.py
+++
@@ -1,4 +0,0 @@
-# DATABASE_URI = "postgresql+psycopg2://postgres:123456@localhost:5432/postgres"
-import os
-
-DATABASE_URI = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:123456@localhost:5432/postgres")
